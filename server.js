331c6c275d2ffca2dcc3c40a6c9a11da3b6d1a3a
Thihakaungset3 <Kaungset138@icloud.com>
2020-05-28 11:22:55 +0630

    change plz

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express()
 
 const supernews = require('./modules/superheroesnews')
 const getComic = require('./modules/getcomic')
+const deadline = require('./modules/deadlinenews')
+
 // Port changes
 const port = process.env.PORT || 1887
 
 app.get('/news', (req, res) => {
-     console.log("Requested");
-     
      supernews.fetchComicsByUniverse()
      .then(r => res.status(200).send(
           {
@@ -18,9 +18,16 @@ app.get('/news', (req, res) => {
      .catch(e => res.status(501).send({message: "Something went wrong"}))
 });
 
-app.get('/comic/universe/:name/:page', (req, res) => {
-     console.log("request");
+app.get('/movie/news', (req, res) => {
+     console.log(req);
      
+     deadline.fetchLatestNews(1)
+          .then(result => res.status(201).send({news: result}))
+          .catch(e => res.status(501).send({message: 'Something went wrong'}))
+});
+
+app.get('/comic/universe/:name/:page', (req, res) => {
+
      let name = req.params.name || marvel
      let page = req.params.page || 1
 
